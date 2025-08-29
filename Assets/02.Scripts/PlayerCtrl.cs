1ca496b11163d0a3ad00d0202920c0b4f04d6866
movinggun0219 <daugun@naver.com>
2025-08-29 15:47:45 +0900

    08/29

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     Transform tr;
+    Animation anim;
+    const float TIME_o = 0.25f;
+    const float INPUT_VALUE = 0.1f;
+    [SerializeField] float moveSpeed = 10.0f;
 
-    [SerializeField]  float moveSpeed = 10.0f;
-    
-    [SerializeField]  float turnSpeed = 80.0f;
+    [SerializeField] float turnSpeed = 80.0f;
     void Start()
     {
         tr = GetComponent<Transform>();
-        
+        anim = GetComponent<Animation>();
+
+        // anim.clip = anim.GetClip("Idle");
+        // anim.Play();
+
+        anim.Play("Idle");
+
     }
 
     // Update is called once per frame
@@ -28,6 +36,31 @@ public class PlayerCtrl : MonoBehaviour
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
         tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
-
+        PlayerAnim(h, v);
     }
+
+    void PlayerAnim(float h, float v)
+    {
+        if (v >= 0.1f)
+        {
+            anim.CrossFade("RunF", 0.25f);
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("RunB", 0.25f);
+        }
+        else if (h >= 0.1f)
+        {
+            anim.CrossFade("RunR", 0.25f);
+        }
+        else if (h < -0.1f)
+        {
+            anim.CrossFade("RunL", 0.25f);
+        }
+        else
+        {
+            anim.CrossFade("Idle", 0.25f);
+        }
+    }
+     
 }
